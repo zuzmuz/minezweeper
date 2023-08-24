5deeb12ab796372919f353a876b7eb61d4088af1
zahersunique <zaher@bandindustries.com>
2023-08-24 19:48:22 +0300

    creating grid

---
+++ src/grid.rs
@@ -0,0 +1,70 @@
+use rand::Rng;
+
+pub struct Grid {
+    grid: Vec<i8>,
+    shape: (usize, usize),
+}
+
+impl Grid {
+    fn set(&mut self, x: usize, y: usize, value: i8) {
+        self.grid[y * self.shape.0 + x] = value;
+    }
+
+    fn increment_cell(&mut self, x: usize, y: usize) {
+        if x < self.shape.0 && y < self.shape.1 && self.get(x, y) != -1 {
+            self.set(x, y, self.get(x, y) + 1);
+        }
+    }
+
+    fn set_mines(&mut self, number_of_mines: usize) {
+        let mut rng = rand::thread_rng();
+        let mut mines = 0;
+        while mines < number_of_mines {
+            let x = rng.gen_range(0..self.shape.0);
+            let y = rng.gen_range(0..self.shape.1);
+            if self.get(x, y) != -1 {
+                self.set(x, y, -1);
+
+                if x > 0 {
+                    self.increment_cell(x - 1, y);
+                    self.increment_cell(x, y + 1);
+                }
+                if y > 0 {
+                    self.increment_cell(x, y - 1);
+                    self.increment_cell(x + 1, y - 1);
+                }
+                if x > 0 && y > 0 {
+                    self.increment_cell(x - 1, y - 1);
+                }
+                self.increment_cell(x + 1, y);
+                self.increment_cell(x, y + 1);
+                self.increment_cell(x + 1, y + 1);
+                mines += 1;
+            }
+        }
+    }
+
+    pub fn new(shape: (usize, usize)) -> Grid {
+        Grid {
+            grid: vec![0; shape.0 * shape.1],
+            shape: shape,
+        }
+    }
+
+    pub fn init(&mut self, number_of_mines: usize) {
+        self.set_mines(number_of_mines);
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> i8 {
+        self.grid[y * self.shape.0 + x]
+    }
+
+    pub fn print(&self) {
+        for y in 0..self.shape.1 {
+            for x in 0..self.shape.0 {
+                print!("{}\t", self.get(x, y));
+            }
+            println!();
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod grid;
+use grid::Grid;
+
 fn main() {
     println!("Hello, world!");
+    let mut grid = Grid::new((10, 10));
+    grid.init(10);
+    grid.print();
 }
